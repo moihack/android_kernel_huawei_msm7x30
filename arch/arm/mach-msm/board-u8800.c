11b185f3a96a8e19e9c0a279532a535014805a99
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-08-26 12:45:57 +0300

    u8800: Remove unnecessary variable in touchscreen detection.

--- arch/arm/mach-msm/board-u8800.c
+++ arch/arm/mach-msm/board-u8800.c
@@ -4487,7 +4487,6 @@ static int __init i2c_touch_init(void)
 	int ret;
 	struct i2c_adapter *touch_i2c_adapter;
 	union i2c_smbus_data data;
-	u8 value;
 
 	touch_i2c_adapter = i2c_get_adapter(0);
 
@@ -4495,7 +4494,6 @@ static int __init i2c_touch_init(void)
 		I2C_SMBUS_BYTE_DATA, &data);
 
 	if (!ret) {
-		value = data.byte;
 		pr_debug("%s: Found Atmel mXT224\n", __func__);
 #if defined(CONFIG_TOUCHSCREEN_ATMEL_MXT)
 		i2c_new_device(touch_i2c_adapter, &atmel_mxt_ts);
